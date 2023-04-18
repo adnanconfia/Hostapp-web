63b0ad8249b431ef7ebf48916c3c839d9f8def92
iammoeez <moeezshahid2022@gmail.com>
2023-04-18 22:26:01 +0500

    User List Display - Some changes in data-grid-comp

--- hostapp/src/app/pages/account/users/users.component.ts
+++ hostapp/src/app/pages/account/users/users.component.ts
@@ -5,7 +5,8 @@ import {
   collection,
   doc,
   updateDoc,
-  deleteDoc
+  deleteDoc,
+  getDocs
 } from '@angular/fire/firestore';
 import { Loader } from './../../../helpers/loader';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
@@ -21,11 +22,11 @@ export class UsersComponent implements OnInit {
   AddUserForm!: FormGroup;
   UpdateForm!: FormGroup;
   cols: any = [];
+  public users: any;
   public roleIdManager: any;
   public roleIdWorker: any;
   public roleIdAdmin: any;
   constructor(private fb: FormBuilder, private firestore: Firestore) {}
-  public users: any;
   ngOnInit(): void {
     this.AddUserForm = this.fb.group({
       firstname: ['', Validators.required],
@@ -34,12 +35,12 @@ export class UsersComponent implements OnInit {
       userrole: ['', Validators.required]
     });
     this.cols = [
-      { header: 'User Name' },
-      { header: 'Role' },
-      { header: 'Eamil' },
-      { header: 'Is Active' },
-      { header: 'Picture' },
-      { header: 'Action' }
+      { header: 'User Name',field: 'name',type:'text'},
+      { header: 'Role',field: 'role',type:'text' },
+      { header: 'Email',field: 'email',type:'text' },
+      { header: 'Is Active',field: 'isActive', type: "status" },
+      { header: 'Picture',field: 'imageUrl', type: "profilePic"},
+      { header: 'Action',field: 'Action', type: "action" }
     ];
     this.users = [
       {
@@ -69,6 +70,39 @@ export class UsersComponent implements OnInit {
     ];
     this.getDataRoleId();
     this.getAllUsers();
+  }
+  async getAllUsers() {
+    Loader.isLoading=true;
+    // Getting Roles
+    var ref = collection(this.firestore, "roles");
+    var roles = await getDocs(ref);
+    let rolesList:any[]=[]
+    roles.forEach((doc)=>{
+      var data = doc.data()
+      rolesList.push(data)
+    })
+    console.log(rolesList)
+    // Getting data from Administrator table
+    const userIns = collection(this.firestore, 'administrators');
+    let qSnap = await getDocs(userIns);
+    let userList:any = []
+    qSnap.forEach((doc)=>{
+      var data = doc.data()
+      data['role']= rolesList.find((r)=> r.roleId=== data['roleId']).name;
+      userList.push(data)
+    })
+
+    this.users= userList
+
+
+    // collectionData(userIns, { idField: 'id' }).subscribe(
+    //   (resp: any) => {
+    //     console.log(resp,"users");
+    //   },
+    //   (error: any) => {
+    //     console.log(error);
+    //   }
+    // );
   }
   onSubmit() {
     if (this.AddUserForm.valid) {
@@ -135,17 +169,7 @@ export class UsersComponent implements OnInit {
         console.log(error);
       });
   }
-  getAllUsers() {
-    const userIns = collection(this.firestore, 'users');
-    collectionData(userIns, { idField: 'id' }).subscribe(
-      (resp: any) => {
-        console.log(resp,"users");
-      },
-      (error: any) => {
-        console.log(error);
-      }
-    );
-  }
+
   deleteUser(id: any) {
     const userDeleteIns = doc(this.firestore, 'users', id);
     deleteDoc(userDeleteIns)
